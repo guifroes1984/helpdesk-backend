224d4bd37bd650463f4b13992cbea094f63d09d4
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-13 19:45:54 -0300

    Anotações JPA para classes de domínio.

--- helpdesk/src/main/java/com/guilherme/helpdesk/domain/Tecnico.java
+++ helpdesk/src/main/java/com/guilherme/helpdesk/domain/Tecnico.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.guilherme.helpdesk.domain.enums.Perfil;
+
 @Entity
 public class Tecnico extends Pessoa {
 
@@ -16,10 +18,12 @@ public class Tecnico extends Pessoa {
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
