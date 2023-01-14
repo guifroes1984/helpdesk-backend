1380ac7c1212da0a21783fb65d6d44a6f111088c
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-14 19:55:26 -0300

    Exceção para objeto não encontrado.

--- helpdesk/src/main/java/com/guilherme/helpdesk/services/TecnicoService.java
+++ helpdesk/src/main/java/com/guilherme/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.guilherme.helpdesk.domain.Tecnico;
 import com.guilherme.helpdesk.repositories.TecnicoRepository;
+import com.guilherme.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,7 +17,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encotrado! Id: " + id));
 	}
 
 }
