cf8541e07aab8b62bd8196b43c42c1d24e625913
guifroes1984 <guilhermefroes26@gmail.com>
2023-01-13 22:29:05 -0300

    Perfil de teste criado.

--- helpdesk/src/main/java/com/guilherme/helpdesk/HelpdeskApplication.java
+++ helpdesk/src/main/java/com/guilherme/helpdesk/HelpdeskApplication.java
@@ -1,51 +1,13 @@
 package com.guilherme.helpdesk;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.guilherme.helpdesk.domain.Chamado;
-import com.guilherme.helpdesk.domain.Cliente;
-import com.guilherme.helpdesk.domain.Tecnico;
-import com.guilherme.helpdesk.domain.enums.Perfil;
-import com.guilherme.helpdesk.domain.enums.Prioridade;
-import com.guilherme.helpdesk.domain.enums.Status;
-import com.guilherme.helpdesk.repositories.ChamadoRepository;
-import com.guilherme.helpdesk.repositories.ClienteRepository;
-import com.guilherme.helpdesk.repositories.TecnicoRepository;
-
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
-	
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-	
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
-	@Autowired
-	private ChamadoRepository chamadoRepository;
+public class HelpdeskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Tecnico tec1 = new Tecnico(null, "Guilherme", "65194617097", "gui@email.com", "123");
-		tec1.addPerfil(Perfil.ADMIN);
-		
-		Cliente cli1 = new Cliente(null, "Fran", "97393381001", "fran@email.com", "123");
-		
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
-		
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
-		
 	}
 
 }
