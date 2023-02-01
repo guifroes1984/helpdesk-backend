74142ffb227ffbc594ef7720dd5e6d27db0616e2
guifroes1984 <guilhermefroes26@gmail.com>
2023-02-01 19:28:38 -0300

    CRUD para Cliente implementado.

--- helpdesk/src/main/java/com/guilherme/helpdesk/domain/Cliente.java
+++ helpdesk/src/main/java/com/guilherme/helpdesk/domain/Cliente.java
@@ -2,11 +2,13 @@ package com.guilherme.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.guilherme.helpdesk.domain.dtos.ClienteDTO;
 import com.guilherme.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -21,6 +23,17 @@ public class Cliente extends Pessoa {
 	public Cliente() {
 		super();
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
